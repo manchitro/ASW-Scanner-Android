e1449be2e8004c32f9e0994e7194d8e5dc4c23fd
Sazid Uddin <43735245+manchitro@users.noreply.github.com>
2020-06-16 15:08:32 +0600

    Dashboard UI complete
    Dashboard is now visitable from login. Created empty scanner activity.

--- app/src/main/java/com/asw/aswscanner/MainActivity.java
+++ app/src/main/java/com/asw/aswscanner/MainActivity.java
@@ -2,13 +2,28 @@ package com.asw.aswscanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
